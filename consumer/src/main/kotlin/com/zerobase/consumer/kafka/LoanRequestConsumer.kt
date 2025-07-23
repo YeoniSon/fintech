20bc6a9914c731b7e7e5686bf866ecb203374157
YeoniSon <sjy8211@gmail.com>
2025-07-23 21:39:00 +0900

    CSS 모듈 -> CB사 모듈 개발 및 연동

--- consumer/src/main/kotlin/com/zerobase/consumer/kafka/LoanRequestConsumer.kt
+++ consumer/src/main/kotlin/com/zerobase/consumer/kafka/LoanRequestConsumer.kt
@@ -1,6 +1,7 @@
 package com.zerobase.consumer.kafka
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.zerobase.consumer.service.LoanRequestService
 import com.zerobase.kafka.dto.LoanRequestDto
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Service
@@ -8,10 +9,13 @@ import org.springframework.stereotype.Service
 @Service
 class LoanRequestConsumer (
     private val objectMapper: ObjectMapper,
-    // TODO : CB 사 호출 로직
+    private val loanRequestService: LoanRequestService
+    //  CB 사 호출 로직
 ){
     @KafkaListener(topics = ["loan_request"], groupId = "fintech")
     fun loanRequestTopicConsumer(message: String) {
         val loanRequestKafkaDto = objectMapper.readValue(message, LoanRequestDto::class.java)
+
+        loanRequestService.loanRequest(loanRequestKafkaDto)
     }
 }
